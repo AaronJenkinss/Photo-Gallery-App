eb7585475d24c88be4966e0c95ede9e0377d3372
Aaron-Jenkins <aaronjenkins22442244@gmail.com>
2023-08-18 02:00:31 +0530

    Styled the scrolling buttons and added configurability for image URLS too.

--- photo-gallery/src/app/page.js
+++ photo-gallery/src/app/page.js
@@ -5,7 +5,14 @@ import ImageCarousel from '@/components/ImageCarousel/ImageCarousel';
 export default function Home() {
   return (
     <div className={styles.main_container}>
-      <ImageCarousel intervalDuration={5000} width={200} height={400} />
+      <ImageCarousel intervalDuration={5000} width={400} height={250} imageURLS={[
+        'https://blog.thermoworks.com/wp-content/uploads/2021/06/Ice_Cream_Compressed-43.jpg',
+        'https://www.alyonascooking.com/wp-content/uploads/2019/08/chocolate-ice-cream-recipe-19.jpg',
+        'https://lh3.googleusercontent.com/LwFcKCcsq1N_fk34nojMA50kCwbdiLVeGmVgElCrWDAK5Jo3VeZZ9aBhLG1hB7IVdqeFvAusP4H7huS8tYCMNA=w1280-h720-c-rj-v1-e365',
+        'https://www.halfbakedharvest.com/wp-content/uploads/2022/07/No-Churn-Chocolate-Peanut-Butter-Ice-Cream-1-500x500.jpg',
+      ]} />
+
+
     </div>
   );
 }
--- photo-gallery/src/components/ImageCarousel/ImageCarousel.js
+++ photo-gallery/src/components/ImageCarousel/ImageCarousel.js
@@ -3,18 +3,20 @@
 import React, { useEffect, useRef, useState } from 'react'
 import styles from './ImageCarousel.module.css';
 import Image from 'next/image';
+import { styleConcat } from '@/utils';
 
-function ImageCarousel({ width, height, intervalDuration }) {
+function ImageCarousel({ width, height, intervalDuration, imageURLS }) {
     const INTERVAL_DURATION = intervalDuration;
     const WIDTH = width;
     const HEIGHT = height;
+    const IMAGE_URLS = imageURLS;
 
     const [offset, setOffset] = useState(0);
     const intervalID = useRef(null);
 
     const setIntervalFunction = () => {
         setOffset(o => {
-            if ((imageURLS.length - 1) * -100 < o) {
+            if ((IMAGE_URLS.length - 1) * -100 < o) {
                 return o - 100;
             }
             else {
@@ -23,14 +25,7 @@ function ImageCarousel({ width, height, intervalDuration }) {
         });
     };
 
-    const imageURLS = [
-        'https://blog.thermoworks.com/wp-content/uploads/2021/06/Ice_Cream_Compressed-43.jpg',
-        'https://www.alyonascooking.com/wp-content/uploads/2019/08/chocolate-ice-cream-recipe-19.jpg',
-        'https://lh3.googleusercontent.com/LwFcKCcsq1N_fk34nojMA50kCwbdiLVeGmVgElCrWDAK5Jo3VeZZ9aBhLG1hB7IVdqeFvAusP4H7huS8tYCMNA=w1280-h720-c-rj-v1-e365',
-        'https://www.halfbakedharvest.com/wp-content/uploads/2022/07/No-Churn-Chocolate-Peanut-Butter-Ice-Cream-1-500x500.jpg',
-    ];
-
-    const images = imageURLS.map((url, index) => {
+    const images = IMAGE_URLS.map((url, index) => {
         return (
             <Image src={url} loader={() => url} width={WIDTH} height={HEIGHT} style={{ left: offset + '%' }} alt='test image.' key={index} />
         );
@@ -44,9 +39,9 @@ function ImageCarousel({ width, height, intervalDuration }) {
         }
     }, []);
 
-    const imageScrollButtons = imageURLS.map((v, i) => {
+    const imageScrollButtons = IMAGE_URLS.map((v, i) => {
         return (
-            <button onClick={() => {
+            <button className={styleConcat(styles.btn, (i === offset / -100 ? styles.highlight_btn : ''))} onClick={() => {
                 clearInterval(intervalID.current);
                 intervalID.current = setInterval(setIntervalFunction, INTERVAL_DURATION);
                 setOffset(i * -100)
@@ -57,7 +52,9 @@ function ImageCarousel({ width, height, intervalDuration }) {
     return (
         <div className={styles.imageCarousel} style={{ width: WIDTH, height: HEIGHT }}>
             <div className={styles.container} style={{ width: WIDTH, height: HEIGHT }}>
-                {imageScrollButtons}
+                <div className={styles.scrollSection}>
+                    {imageScrollButtons}
+                </div>
             </div>
             {images}
         </div>
