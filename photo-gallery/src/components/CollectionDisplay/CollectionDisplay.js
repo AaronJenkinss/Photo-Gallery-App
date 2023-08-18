809f2653d743c37310387565cbcb2e5cdf4005a9
Aaron-Jenkins <aaronjenkins22442244@gmail.com>
2023-08-18 23:29:50 +0530

    A few more changes to the image collection component, mainly, starting it when mouse is over the component and stopping it when it is no longer over the component.

--- photo-gallery/src/components/CollectionDisplay/CollectionDisplay.js
+++ photo-gallery/src/components/CollectionDisplay/CollectionDisplay.js
@@ -6,7 +6,7 @@ import styles from './CollectionDisplay.module.css';
 function CollectionDisplay() {
     return (
         <div className={styles.collectionDisplay}>
-            <ImageCollection widthAndHeight={250} imageURLS={[
+            <ImageCollection widthAndHeight={150} imageURLS={[
                 'https://blog.thermoworks.com/wp-content/uploads/2021/06/Ice_Cream_Compressed-43.jpg',
                 'https://www.alyonascooking.com/wp-content/uploads/2019/08/chocolate-ice-cream-recipe-19.jpg',
                 'https://lh3.googleusercontent.com/LwFcKCcsq1N_fk34nojMA50kCwbdiLVeGmVgElCrWDAK5Jo3VeZZ9aBhLG1hB7IVdqeFvAusP4H7huS8tYCMNA=w1280-h720-c-rj-v1-e365',
--- photo-gallery/src/components/ImageCollection/ImageCollection.js
+++ photo-gallery/src/components/ImageCollection/ImageCollection.js
@@ -10,7 +10,7 @@ function ImageCollection({ widthAndHeight, imageURLS }) {
     const INITIAL_ROTATION = -20;
     const ROTATION_OFFSET = 10;
     const INTERVAL_DURATION = 1000;
-
+    const BOTTOM_OFFSET = '190%';
     const IMAGE_URLS = imageURLS;
 
     const [imageOffsets, setImageOffsets] = useState(IMAGE_URLS.map((value, index) => {
@@ -18,56 +18,57 @@ function ImageCollection({ widthAndHeight, imageURLS }) {
     }));
 
     const [currentIndex, setCurrentIndex] = useState(IMAGE_URLS.length - 1);
-    const currentIteration = useRef(0);
+    const [toggleAnimation, setToggleAnimation] = useState(false);
 
     const images = IMAGE_URLS.map((url, index) => {
         return (
-            <Image src={url} style={{ transform: 'rotate(' + (imageOffsets[index].rotation) + 'deg)', zIndex: imageOffsets[index].zIndex, bottom: (index === currentIndex ? '150%' : '0px') }}
+            <Image src={url} style={{ transform: 'rotate(' + (imageOffsets[index].rotation) + 'deg)', zIndex: imageOffsets[index].zIndex, bottom: (index === currentIndex && toggleAnimation ? BOTTOM_OFFSET : '0px') }}
                 width={WIDTH_AND_HEIGHT} height={WIDTH_AND_HEIGHT} unoptimized={true} key={index} alt='test images.'
             />
         );
-    }).reverse();
+    });
 
-    useEffect(() => {
-        const intervalID = setInterval(() => {
-            setCurrentIndex(ci => {
-                if (ci === 0) {
-                    return IMAGE_URLS.length - 1;
-                }
-                else {
-                    return ci - 1;
-                }
-            });
+    const intervalID = useRef(null);
+
+    const nextIndex = ci => {
+        if (ci === 0) {
+            return IMAGE_URLS.length - 1;
+        }
+        else {
+            return ci - 1;
+        }
+    };
+    const startAnimation = () => {
+        setToggleAnimation(true);
+        intervalID.current = setInterval(() => {
+            setCurrentIndex(nextIndex);
         }, INTERVAL_DURATION);
-
-        return () => {
-            clearInterval(intervalID);
-        };
-    }, []);
+    };
+    const stopAnimation = () => {
+        setToggleAnimation(false);
+        setCurrentIndex(nextIndex);
+        clearInterval(intervalID.current);
+    };
 
     useEffect(() => {
-
-
         setImageOffsets(ro => {
+            if (toggleAnimation) {
+                ro.map((offset) => {
+                    offset.rotation = offset.rotation + ROTATION_OFFSET;
+                    offset.zIndex = offset.zIndex + 1;
+                    return offset;
+                });
 
-            const currentRotationOffset = currentIteration.current * ROTATION_OFFSET;
-            ro.map((offset) => {
-                offset.rotation = offset.rotation + ROTATION_OFFSET;
-                offset.zIndex = offset.zIndex + 1;
-                return offset;
-            });
-
-            currentIteration.current++;
-
-            ro[currentIndex].rotation = INITIAL_ROTATION;
-            ro[currentIndex].zIndex = 0;
+                ro[currentIndex].rotation = INITIAL_ROTATION;
+                ro[currentIndex].zIndex = 0;
+            }
 
             return ro;
         });
-    }, [currentIndex]);
+    }, [currentIndex, toggleAnimation]);
 
     return (
-        <div className={styles.imageCollection} style={{ width: widthAndHeight + 'px', height: widthAndHeight + 'px' }}>
+        <div className={styles.imageCollection} style={{ width: widthAndHeight + 'px', height: widthAndHeight + 'px' }} onMouseEnter={() => startAnimation()} onMouseLeave={stopAnimation}>
             {images}
         </div>
     )
